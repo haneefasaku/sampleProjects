21f40727e44198a8a8b32539343ed9a052488730
jamsheer adiyantakath <jamshe.jamsheer@gmail.com>
2024-12-13 16:07:39 +0530

    connect mongodb

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from 'express'
+import { connectDB } from './config/db.js'
 const app = express()
 const port = 3000
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
